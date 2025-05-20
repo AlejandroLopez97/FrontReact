8eafaf9730a509905f10a3b16c9245a7d9867693
Alejandro-Oliveros <alejandro.lopez@pascualbravo.edu.co>
2025-05-20 15:31:42 -0500

    Se crea el componente task y task list como componente contenedor

---
+++ src/components/pure/task.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Task } from '../../models/task.class';
+
+const TaskComponent = ({task}) => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+
+TaskComponent.propTypes = {
+    task: PropTypes.instanceOf(Task)
+};
+
+
+export default TaskComponent;
