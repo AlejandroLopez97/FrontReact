d6fe950e8b0fd3b776bc43f3f222b043d9d00942
Alejandro-Oliveros <alejandro.lopez@pascualbravo.edu.co>
2025-05-28 11:44:02 -0500

    Manejo del ciclo de vida en los componentes creados anteriormente

--- src/components/container/task_list.jsx
+++ src/components/container/task_list.jsx
@@ -1,15 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Task } from '../../models/task.class'
 import { Levels } from '../../models/levels.enum';
 import TaskComponent from '../pure/task';
 
 function TaskListComponent() {
 
-    const defaultTask = new Task('Example', 'Default description', false, Levels.URGENT);
+  const defaultTask = new Task('Example', 'Default description', false, Levels.URGENT);
+  //Estado del componente 
+  const [tasks, setTasks] = useState([defaultTask]);
+  const [loading, setLoading] = useState(true);
 
-    const changeState = (id) => {
-        console.log('TODO: Cambiar estado de una tarea');
-    }       
+  //control del ciclo de vida del componente
+  useEffect(() => {
+    console.log('Task State has been modified');
+    setLoading(false);
+    return () =>{
+      console.log('TaskList component is going to unmount...')
+    }
+  }, [tasks]);
+
+
+  const changeCompleted = (id) => {
+      console.log('TODO: Cambiar estado de una tarea');
+  }       
 
   return (
     <div>
